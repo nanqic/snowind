a9878d8aac37d3fb34b142fb9a6cd6b87f79e18b
nanqic <nanqic@163.com>
2018-09-01 12:42:32 +0800

    init files

---
+++ app/controller/db_config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'DB_HOST' => 'localhost',
+	'DB_USER' =>'root',
+	'DB_PWD' => 'xn1989',
+	'DB_NAME' => 'sw_blog',
+	'DB_CHARSET' => 'utf8',
+	'DB_PREFIX' => '',
+];
---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$config = include '../config/config.php';
+include '../bootstrap/Psr4AutoLoad.php';
+include '../bootstrap/Start.php';
+include '../bootstrap/alias.php';
+Start::router();
+
+
+
+
